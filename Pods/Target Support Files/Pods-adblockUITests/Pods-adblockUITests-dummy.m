d69f23f26328df24c2c1b0f9caccd95a05ff4fb7
森下裕貴 <ather.wesker818@icloud.com>
2018-08-17 08:40:31 +0900

    Pods

---
+++ Pods/Target Support Files/Pods-adblockUITests/Pods-adblockUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_adblockUITests : NSObject
+@end
+@implementation PodsDummy_Pods_adblockUITests
+@end
---
+++ Pods/Target Support Files/Pods-blockList/Pods-blockList-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_blockList : NSObject
+@end
+@implementation PodsDummy_Pods_blockList
+@end

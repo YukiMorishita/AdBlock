3fb2b832783a7582630417db0d5415e2fb6c1f98
admin <admin@ht16a087.local>
2018-11-30 06:25:06 +0900

    update  blockerList

--- ContentBlockerExtension/ContentBlockerRequestHandler.swift
+++ ContentBlockerExtension/ContentBlockerRequestHandler.swift
@@ -9,15 +9,57 @@
 import UIKit
 import MobileCoreServices
 
-class ContentBlockerRequestHandler: NSObject, NSExtensionRequestHandling {
+enum BlockerError: Error {
+    case LoadFailed
+}
 
+class ContentBlockerRequestHandler: NSObject, NSExtensionRequestHandling {
+    
+    private let groupID: String = "group.jp.ac.osakac.cs.hisalab.adblock"
+    
+    func getJsonFileName() -> String {
+        
+        var jsonFileName = ""
+        let containerURL = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: groupID)
+        
+        do {
+            let containerDir = try FileManager.default.contentsOfDirectory(at: containerURL!, includingPropertiesForKeys: nil)
+            let dir = containerDir.map { $0.lastPathComponent }.filter { $0.contains(".json") }
+            
+            if dir.isEmpty == false {
+                jsonFileName = dir.last!
+            }
+            
+            if dir.isEmpty == true {
+                jsonFileName = "blockerList.json"
+            }
+        } catch let error as NSError{
+            print("failed to json file: \(error)")
+        }
+        
+        return jsonFileName
+    }
+    
     func beginRequest(with context: NSExtensionContext) {
-        let attachment = NSItemProvider(contentsOf: Bundle.main.url(forResource: "blockerList", withExtension: "json"))!
         
-        let item = NSExtensionItem()
-        item.attachments = [attachment]
+        let jsonFileName = getJsonFileName()
+        let containerURL = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: groupID)?.appendingPathComponent(jsonFileName)
+        let fileExists = FileManager.default.fileExists(atPath: containerURL!.path)
         
-        context.completeRequest(returningItems: [item], completionHandler: nil)
+        if fileExists {
+            let attachment = NSItemProvider(contentsOf: containerURL)
+            
+            let item = NSExtensionItem()
+            item.attachments = [attachment] as? [NSItemProvider]
+            
+            context.completeRequest(returningItems: [item], completionHandler: nil)
+        }
+        
+        if !fileExists {
+            print("Could not load the blocker list JSON")
+            context.cancelRequest(withError: BlockerError.LoadFailed)
+        }
+        
     }
     
 }
